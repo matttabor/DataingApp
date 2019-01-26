9a89a64c718bd16a3fb1d56e4a1cff69f488e142
Matthew Tabor <mctabor@gmail.com>
2019-01-26 09:00:51 -0800

    end of section 16

--- DatingApp.API/Controllers/MessagesController.cs
+++ DatingApp.API/Controllers/MessagesController.cs
@@ -47,7 +47,8 @@ namespace DatingApp.API.Controllers
         [HttpPost]
         public async Task<IActionResult> CreateMessage(int userId, MessageForCreationDto messageForCreationDto)
         {
-            if (userId != int.Parse(User.FindFirst(ClaimTypes.NameIdentifier).Value))
+            var sender = await _repository.GetUser(userId);
+            if (sender.Id != int.Parse(User.FindFirst(ClaimTypes.NameIdentifier).Value))
             {
                 return Unauthorized();
             }
@@ -63,14 +64,48 @@ namespace DatingApp.API.Controllers
             var message = _mapper.Map<Message>(messageForCreationDto);
 
             _repository.Add(message);
-            var messageToReturn = _mapper.Map<MessageForCreationDto>(message);
+            
 
             if(await _repository.SaveAll())
             {
+                var messageToReturn = _mapper.Map<MessageToReturnDto>(message);
                 return CreatedAtRoute("GetMessage", new {id = message.Id }, messageToReturn);
             }
 
             throw new Exception("Creating the message failed on save");
+        }
+
+        [HttpPost("{id}")]
+        public async Task<IActionResult> DeleteMessage(int id, int userId)
+        {
+            if (userId != int.Parse(User.FindFirst(ClaimTypes.NameIdentifier).Value))
+            {
+                return Unauthorized();
+            }
+
+            var messageFromRepo = await _repository.GetMessage(id);
+
+            if (messageFromRepo.SenderId == userId)
+            {
+                messageFromRepo.SenderDeleted = true;
+            }
+
+            if(messageFromRepo.RecipientId == userId)
+            {
+                messageFromRepo.RecipientDeleted = true;
+            }
+
+            if(messageFromRepo.SenderDeleted && messageFromRepo.RecipientDeleted)
+            {
+                _repository.Delete(messageFromRepo);
+            }
+
+            if(await _repository.SaveAll())
+            {
+                return NoContent();
+            }
+
+            throw new Exception("Error deleting the message");
         }
     
         [HttpGet]
@@ -105,6 +140,29 @@ namespace DatingApp.API.Controllers
             var messageThread = _mapper.Map<IEnumerable<MessageToReturnDto>>(messageFromRepo);
 
             return Ok(messageThread);
+        }
+
+        [HttpPost("{id}/read")]
+        public async Task<IActionResult> MarkAsRead(int userId, int id)
+        {
+            if (userId != int.Parse(User.FindFirst(ClaimTypes.NameIdentifier).Value))
+            {
+                return Unauthorized();
+            }
+
+            var message = await _repository.GetMessage(id);
+
+            if(message.RecipientId != userId)
+            {
+                return Unauthorized();
+            }
+
+            message.IsRead = true;
+            message.DateRead = DateTime.Now;
+
+            await _repository.SaveAll();
+
+            return NoContent();
         }
     }
 }
--- DatingApp.API/Data/DatingRepository.cs
+++ DatingApp.API/Data/DatingRepository.cs
@@ -132,13 +132,13 @@ namespace DatingApp.API.Data
             switch(messageParams.MessageContainer)
             {
                 case "Inbox":
-                    messages = messages.Where(u=> u.RecipientId == messageParams.UserId);
+                    messages = messages.Where(u=> u.RecipientId == messageParams.UserId && !u.RecipientDeleted);
                     break;
                 case "Outbox":
-                    messages = messages.Where(u=> u.SenderId == messageParams.UserId);
+                    messages = messages.Where(u=> u.SenderId == messageParams.UserId && !u.SenderDeleted);
                     break;
                 default: 
-                    messages = messages.Where(u=> u.RecipientId == messageParams.UserId && !u.IsRead);
+                    messages = messages.Where(u=> u.RecipientId == messageParams.UserId && !u.IsRead && !u.RecipientDeleted);
                     break;
             }
 
@@ -152,7 +152,8 @@ namespace DatingApp.API.Data
             var messages = await _context.Messages
                .Include(u => u.Sender).ThenInclude(p => p.Photos)
                .Include(u => u.Recipient).ThenInclude(p => p.Photos)
-               .Where(m => m.RecipientId == userId && m.SenderId == recipientId || m.RecipientId == recipientId && m.SenderId == userId)
+               .Where(m => m.RecipientId == userId &&!m.RecipientDeleted && m.SenderId == recipientId 
+                || m.RecipientId == recipientId && m.SenderId == userId && m.SenderDeleted == false)
                .OrderByDescending(m=> m.MessageSent)
                .ToListAsync();
 
